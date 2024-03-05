9393aff7815f9b3b8bbabe6de0863d9d477fb026
yeong-hwan <jghff700@naver.com>
2024-03-06 01:50:45 +0900

    feat(week1): test code for practice h2 DB & API call

---
+++ week1/src/main/java/com/ceos19/springboot/Application.java
@@ -0,0 +1,34 @@
+package com.ceos19.springboot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			// Spring Boot 에서 제공되는 Bean 확인
+//			String[] beanNames = ctx.getBeanDefinitionNames();
+//			Arrays.sort(beanNames);
+//			for (String beanName : beanNames) {
+//				System.out.println(beanName);
+//			}
+
+		};
+	}
+
+}
