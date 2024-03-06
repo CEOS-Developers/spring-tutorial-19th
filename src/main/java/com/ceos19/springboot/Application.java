0d85bd5c80423dad2c559a005bbcda1484ba652a
Jinu Yi <dionisos198@naver.com>
2024-03-06 23:46:48 +0900

    Setting: init

---
+++ src/main/java/com/ceos19/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.ceos19.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
