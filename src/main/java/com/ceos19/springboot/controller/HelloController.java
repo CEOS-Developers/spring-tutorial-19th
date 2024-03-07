84492f142834a7e5f4d7662c2b8d30cc882bcd7f
kwon <kckc0608@naver.com>
2024-03-07 16:03:07 +0900

    refactor: controller package로 묶어서 분리

--- src/main/java/com/ceos19/springboot/controller/HelloController.java
+++ src/main/java/com/ceos19/springboot/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.ceos19.springboot;
+package com.ceos19.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/ceos19/springboot/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.ceos19.springboot;
+
+import com.ceos19.springboot.service.TestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/tests")
+public class TestController {
+    private final TestService testService;
+
+    @GetMapping
+    public List<Test> findAllTests() {
+        return testService.findAllTests();
+    }
+}
