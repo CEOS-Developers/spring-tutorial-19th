3c05957162d04854d49a9a357c19c237b4ebbf8f
Min Jeon <121627245+riceCakeSsamanKo@users.noreply.github.com>
2024-03-09 21:48:50 +0900

    [19기_전민 spring tutorial 미션 제출합니다.

---
+++ spring-boot/src/main/java/com/ceos19/springboot/repository/TestRepository.java
@@ -0,0 +1,7 @@
+package com.ceos19.springboot.repository;
+
+import com.ceos19.springboot.domain.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestRepository extends JpaRepository<Test,Long> {
+}
